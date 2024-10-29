84626a4c81582c4d9cc6b74ed1f3c92d85bf517e
AxalotL <ladyginnic815@gmail.com>
2024-10-29 15:03:40 +0500

    Optimize code

--- src/main/java/com/axalotl/async/mixin/world/ServerChunkManagerMixin.java
+++ src/main/java/com/axalotl/async/mixin/world/ServerChunkManagerMixin.java
@@ -1,11 +1,7 @@
 package com.axalotl.async.mixin.world;
 
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.server.world.ServerChunkManager;
-import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkManager;
-import net.minecraft.world.chunk.ChunkStatus;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,10 +14,5 @@ public abstract class ServerChunkManagerMixin extends ChunkManager {
     @Redirect(method = {"getChunk(IILnet/minecraft/world/chunk/ChunkStatus;Z)Lnet/minecraft/world/chunk/Chunk;", "getWorldChunk"}, at = @At(value = "FIELD", target = "Lnet/minecraft/server/world/ServerChunkManager;serverThread:Ljava/lang/Thread;", opcode = Opcodes.GETFIELD))
     private Thread overwriteServerThread(ServerChunkManager mgr) {
         return Thread.currentThread();
-    }
-
-    @WrapMethod(method = "putInCache")
-    private synchronized void syncPutInCache(long pos, Chunk chunk, ChunkStatus status, Operation<Void> original) {
-        original.call(pos, chunk, status);
     }
 }
