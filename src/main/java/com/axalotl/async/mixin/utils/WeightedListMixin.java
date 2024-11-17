7cd39074991f410c981523084b037605f60b9c91
axalotl <ladyginnic815@gmail.com>
2024-11-17 20:49:15 +0500

    Fix WeightedList

---
+++ src/main/java/com/axalotl/async/mixin/utils/WeightedListMixin.java
@@ -0,0 +1,15 @@
+package com.axalotl.async.mixin.utils;
+
+import net.minecraft.util.collection.WeightedList;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Mixin(WeightedList.class)
+public abstract class WeightedListMixin<U> implements Iterable<U> {
+    @Shadow @Final protected List<WeightedList.Entry<U>> entries = new CopyOnWriteArrayList<>();
+
+}
