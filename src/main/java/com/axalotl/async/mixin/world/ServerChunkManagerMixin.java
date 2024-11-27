bcc031f05b3e5ed17636e2a17ea09d324798968a
axalotl <ladyginnic815@gmail.com>
2024-11-27 20:20:59 +0500

    Fix chunks && Fix teleport bug

--- src/main/java/com/axalotl/async/mixin/world/ServerChunkManagerMixin.java
+++ src/main/java/com/axalotl/async/mixin/world/ServerChunkManagerMixin.java
@@ -1,18 +1,14 @@
 package com.axalotl.async.mixin.world;
 
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.server.world.*;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.chunk.*;
 import org.jetbrains.annotations.Nullable;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.concurrent.CompletableFuture;
@@ -30,16 +26,6 @@ public abstract class ServerChunkManagerMixin extends ChunkManager {
     @Shadow
     @Final
     public ServerChunkLoadingManager chunkLoadingManager;
-
-    @Redirect(method = {"getChunk(IILnet/minecraft/world/chunk/ChunkStatus;Z)Lnet/minecraft/world/chunk/Chunk;", "getWorldChunk"}, at = @At(value = "FIELD", target = "Lnet/minecraft/server/world/ServerChunkManager;serverThread:Ljava/lang/Thread;", opcode = Opcodes.GETFIELD))
-    private Thread overwriteServerThread(ServerChunkManager mgr) {
-        return Thread.currentThread();
-    }
-
-    @WrapMethod(method = "putInCache")
-    private synchronized void syncPutInCache(long pos, Chunk chunk, ChunkStatus status, Operation<Void> original) {
-        original.call(pos, chunk, status);
-    }
 
     @Inject(method = "getChunk(IILnet/minecraft/world/chunk/ChunkStatus;Z)Lnet/minecraft/world/chunk/Chunk;", at = @At("HEAD"), cancellable = true)
     private void shortcutGetChunk(int x, int z, ChunkStatus leastStatus, boolean create, CallbackInfoReturnable<Chunk> cir) {
